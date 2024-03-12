80e82244048cd95ed08fffb97ecb30f442ac0c95
Anastasia <an.korobeynikova@gmail.com>
2024-03-12 19:45:13 +0300

    made file with colors on pages

--- src/components/components/main/allProducts/AllProductsMain.tsx
+++ src/components/components/main/allProducts/AllProductsMain.tsx
@@ -1,10 +1,14 @@
 import {Header} from "@/components/components/main/header/Header";
 import {AllProductsInterfaceProps} from "@/components/interfaces/allProductsInterface";
+import main_scss from './AllProductsMain.module.scss'
 
 export const AllProductsMain = (props: AllProductsInterfaceProps) => {
     return (
-        <main>
-            <Header/>
-        </main>
+        <section className={main_scss.route}>
+            <main className={main_scss.page}>
+                <Header/>
+            </main>
+
+        </section>
     )
 }
--- src/components/components/main/header/Header.tsx
+++ src/components/components/main/header/Header.tsx
@@ -1,7 +1,8 @@
+import header_scss from './Header.module.scss'
 export const Header = () => {
     return (
-        <header>
-
+        <header className={header_scss.route}>
+            <div className={header_scss.title}>QPICK</div>
         </header>
     )
 }
